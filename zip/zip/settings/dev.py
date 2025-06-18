6279d6664d5fcd6342b1c8627983887919dfb45e
LepkoQQ <lepko.san@gmail.com>
2025-06-18 14:27:02 +0200

    start wagtail project and add django gitignore

---
+++ zip/zip/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-8mc6dyl=w!h4rjda%t^)x=gvc3#t4j^^(@*jsmzj(%)bcgt5w="
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
