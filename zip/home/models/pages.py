7bf1caaeaca0d2d24f708df06d7a400f9e11b21a
LepkoQQ <lepko.san@gmail.com>
2025-06-24 14:41:59 +0200

    add page models stubs, remove some default image formats

--- zip/home/models/pages.py
+++ zip/home/models/pages.py
@@ -1,5 +1,77 @@
+from django.utils.translation import gettext_lazy as _
 from wagtail.models import Page
 
 
 class HomePage(Page):
-    pass
+    parent_page_types = []
+    max_count = 1
+
+    class Meta:
+        verbose_name = _("Home page")
+        verbose_name_plural = _("Home pages")
+
+
+class EventListPage(Page):
+    parent_page_types = ["HomePage"]
+    max_count = 1
+
+    class Meta:
+        verbose_name = _("Event list")
+        verbose_name_plural = _("Event lists")
+
+
+class EventPage(Page):
+    parent_page_types = ["EventListPage"]
+
+    class Meta:
+        verbose_name = _("Event")
+        verbose_name_plural = _("Events")
+
+
+class ProjectListPage(Page):
+    parent_page_types = ["HomePage"]
+    max_count = 1
+
+    class Meta:
+        verbose_name = _("Project list")
+        verbose_name_plural = _("Project lists")
+
+
+class ProjectPage(Page):
+    parent_page_types = ["ProjectListPage"]
+
+    class Meta:
+        verbose_name = _("Project")
+        verbose_name_plural = _("Projects")
+
+
+class NewsListPage(Page):
+    parent_page_types = ["HomePage"]
+    max_count = 1
+
+    class Meta:
+        verbose_name = _("News list")
+        verbose_name_plural = _("News lists")
+
+
+class NewsPage(Page):
+    parent_page_types = ["NewsListPage"]
+
+    class Meta:
+        verbose_name = _("News post")
+        verbose_name_plural = _("News posts")
+
+
+class AboutUsPage(Page):
+    parent_page_types = ["HomePage"]
+    max_count = 1
+
+    class Meta:
+        verbose_name = _("About us")
+        verbose_name_plural = _("About us")
+
+
+class GenericPage(Page):
+    class Meta:
+        verbose_name = _("Generic page")
+        verbose_name_plural = _("Generic pages")
