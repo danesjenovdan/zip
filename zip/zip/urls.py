c4e2465e15411871e16c1bdac72a5b8099ca2b87
LepkoQQ <lepko.san@gmail.com>
2025-07-31 12:59:57 +0200

    add favicon and og image

--- zip/zip/urls.py
+++ zip/zip/urls.py
@@ -1,12 +1,15 @@
 from django.conf import settings
 from django.conf.urls.i18n import i18n_patterns
 from django.contrib import admin
+from django.templatetags.static import static
 from django.urls import include, path
+from django.views.generic.base import RedirectView
 from wagtail import urls as wagtail_urls
 from wagtail.admin import urls as wagtailadmin_urls
 from wagtail.documents import urls as wagtaildocs_urls
 
 urlpatterns = [
+    path("favicon.ico", RedirectView.as_view(url=static("img/favicon.ico"))),
     path("django-admin/", admin.site.urls),
     path("admin/", include(wagtailadmin_urls)),
     path("documents/", include(wagtaildocs_urls)),
